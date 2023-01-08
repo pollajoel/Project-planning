ba87b67b9c29d4ba661461728506690f68810627
pollajoel2017@gmail.com <pollajoel2017@gmail.com>
2023-01-08 17:54:09 +0100

    (main): add config/bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
